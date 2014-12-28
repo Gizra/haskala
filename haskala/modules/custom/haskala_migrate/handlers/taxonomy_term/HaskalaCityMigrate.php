849cb04b07cb37f0ffe1f59d429887ede5a470a1
Miri Meltzer <miri@gizra.com>
2014-12-28 11:39:07 +0200

    continue 2

---
+++ haskala/modules/custom/haskala_migrate/handlers/taxonomy_term/HaskalaCityMigrate.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Contains \HaskalaCityTermsMigrate.
+ */
+class HaskalaCityTermsMigrate extends HaskalaMigration {
+  public $entityType = 'taxonomy_term';
+  public $bundle = 'city';
+}
