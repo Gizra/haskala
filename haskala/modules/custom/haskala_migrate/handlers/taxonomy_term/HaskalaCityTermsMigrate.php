cb456373e72d614b5adf45a95138dfab4297e420
Itamar Shapira Bar-Lev <itamar@gizra.com>
2014-12-30 13:37:53 +0200

    #2: Migrating cities

---
+++ haskala/modules/custom/haskala_migrate/handlers/taxonomy_term/HaskalaCityTermsMigrate.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \HaskalaCityTermsMigrate.
+ */
+
+class HaskalaCityTermsMigrate extends HaskalaMigration {
+  public $entityType = 'taxonomy_term';
+  public $bundle = 'cities';
+}
+
