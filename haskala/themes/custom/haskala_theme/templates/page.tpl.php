4a51c5572c82dac8942935ca563bed663ce8967b
Miri Meltzer <miri@gizra.com>
2014-12-31 10:53:29 +0200

    Print variable

--- haskala/themes/custom/haskala_theme/templates/page.tpl.php
+++ haskala/themes/custom/haskala_theme/templates/page.tpl.php
@@ -33,7 +33,7 @@
 
   <footer>
     <nav class="icons-nav nav-bar">
-      <?php print drupal_render(menu_tree('menu-icons-menu'));  ?>
+      <?php print render($icons_menu);  ?>
     </nav>
     <div class="login-area">
       <?php print render($user_menu);  ?>
