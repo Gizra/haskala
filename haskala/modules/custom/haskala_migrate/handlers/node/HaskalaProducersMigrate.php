5841330b2d9f8cabdd4975967878e881e2128696
Miri Meltzer <miri@gizra.com>
2015-01-07 08:32:16 +0200

    Migrate continue

--- haskala/modules/custom/haskala_migrate/handlers/node/HaskalaProducersMigrate.php
+++ haskala/modules/custom/haskala_migrate/handlers/node/HaskalaProducersMigrate.php
@@ -9,10 +9,11 @@ class HaskalaProducersMigrate extends HaskalaMigration {
 
   public $csvColumns = array(
     array('field_book', 'Producer of book'),
-    array('field_producer', 'Title'),
+    array('field_producer', 'Name of book producer'),
     array('field_role', 'Role in book creation'),
     array('field_roles_notes', "Notes on roles in book's creation"),
     array('field_person_name_appear', "Does the person's name appear in the book"),
+    array('field_name_in_book', 'Name as it appears in the book'),
     array('field_producer_evidence', "Evidence for person's identity if it does not appear in the book"),
     array('field_writing_age', 'Approximate age during writing of book'),
     array('field_religion', 'Religion during writing of book'),
@@ -31,27 +32,39 @@ class HaskalaProducersMigrate extends HaskalaMigration {
     $this->addFieldMapping('field_book', 'field_book')
       ->callbacks(array($this, 'getBookNid'));
 
-    $this->addFieldMapping('field_language', 'field_language')
+    $this->addFieldMapping('field_producer', 'field_producer')
+      ->callbacks(array($this, 'getPersonNid'));
+
+    $this->addFieldMapping('field_role', 'field_role')
       ->separator(',');
-    $this->addFieldMapping('field_language:create_term')
+    $this->addFieldMapping('field_role:create_term')
       ->defaultValue(TRUE);
-    $this->addFieldMapping('field_language:ignore_case')
+    $this->addFieldMapping('field_role:ignore_case')
       ->defaultValue(TRUE);
 
-    $this->addFieldMapping('field_translation_year', 'field_translation_year');
+    $this->addFieldMapping('field_roles_notes', 'field_roles_notes');
 
-    $this->addFieldMapping('field_translation_city', 'field_translation_city')
+    $this->addFieldMapping('field_person_name_appear', 'field_person_name_appear');
+
+    $this->addFieldMapping('field_producer_evidence', 'field_producer_evidence');
+
+    $this->addFieldMapping('field_writing_age', 'field_writing_age');
+
+    $this->addFieldMapping('field_religion', 'field_religion');
+    $this->addFieldMapping('field_religion:create_term')
+      ->defaultValue(TRUE);
+    $this->addFieldMapping('field_religion:ignore_case')
+      ->defaultValue(TRUE);
+
+    $this->addFieldMapping('field_writing_location', 'field_writing_location')
       ->separator(',');
-    $this->addFieldMapping('field_translation_city:create_term')
+    $this->addFieldMapping('field_writing_location:create_term')
       ->defaultValue(TRUE);
-    $this->addFieldMapping('field_translation_city:ignore_case')
+    $this->addFieldMapping('field_writing_location:ignore_case')
       ->defaultValue(TRUE);
 
-    $this
-      ->addFieldMapping('field_translator', 'field_translator')
-      ->sourceMigration('HaskalaPeopleMigrate');
+    $this->addFieldMapping('field_person_book_references', 'field_person_book_references');
 
-    $this->addFieldMapping('field_translation_references', 'field_translation_references');
   }
 
   protected function getBookNid($value) {
@@ -69,4 +82,22 @@ class HaskalaProducersMigrate extends HaskalaMigration {
     return $value;
   }
 
+  protected function getPersonNid($value) {
+    $query = new EntityFieldQuery();
+    $node_list = $query->entityCondition('entity_type', 'node')
+      ->propertyCondition('type', 'person')
+      ->propertyCondition('title', $value)
+      ->execute();
+
+    if (isset($node_list['node'])) {
+      $node_nid_list = array_keys($node_list['node']);
+      $value = $node_nid_list['0'];
+    }
+
+    return $value;
+  }
+
+  public function prepareRow($row) {
+     dpm($row);
+  }
 }
