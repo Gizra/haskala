3dab7ac8ebcc0331f8fbd31363b55e71b3ec0401
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-01-12 15:30:17 +0200

    Merging the two behat folders into one

--- haskala/behat/aliases.drushrc.php
+++ haskala/behat/aliases.drushrc.php
@@ -1,6 +1,6 @@
 <?php
 
-$aliases['haskala'] = array(
+$aliases['site'] = array(
   'uri' => 'http://127.0.0.1:8080',
   'root' => '/home/travis/build/Gizra/haskala/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
