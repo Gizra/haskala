34b0be6d9db4b4588b155be5e9daa7215176e21e
Miri Meltzer <miri@gizra.com>
2015-01-14 10:52:05 +0200

    Book groups plugin

--- haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
@@ -86,36 +86,39 @@
           <h1 class="title page-header"><?php print $title; ?></h1>
         <?php endif; ?>
 
-        <div class="content">
           <?php
           //print render($content);
           ?>
-          <div class="top-fields">
-            <div class="field">
-              <div class="field-label type-label">Type of book:</div>
-              <div class="content"></div>
-            </div>
-            <?php foreach($top_details_group as $field){
-              //dpm($field);
-              print drupal_render($field);
-            }
-            ?>
-            <!--<div class="field">
-              <div class="field-label">Full title of book:</div>
-              <div class="content">Abhandlung von der Freiheit des Menschen</div>
-            </div>-->
+        <div class="top-fields">
+          <div class="field">
+            <div class="field-label type-label">Type of book:</div>
+            <div class="content"></div>
           </div>
+          <?php foreach($top_details_group as $field):
+            print render($field);
+          endforeach; ?>
         </div>
       </section>
       <section class="bottom-details">
-        <div>
-          <?php foreach ($groups as $group){
-            foreach ($group as $group_field) {
-              print drupal_render($group_field);
-            }
-          } ?>
-
-        </div>
+        <nav class="details-navigation">
+          <ul>
+            <?php foreach($groups as $group_name => $group): ?>
+                <li><a href="#<?php print $group_name; ?>"><?php print $group_name; ?></a></li>
+            <?php endforeach; ?>
+          </ul>
+        </nav>
+        <section class="groups">
+          <?php foreach($groups as $group_name => $group): ?>
+              <div class="fields-group">
+                <div class="fields-group-name" id="<?php print $group_name; ?>"><?php print $group_name; ?></div>
+                <div class="fields">
+                  <?php foreach($group as $group_field):
+                    print render($group_field);
+                  endforeach; ?>
+                </div>
+              </div>
+          <?php endforeach; ?>
+        </section>
       </section>
 
 
