81f74015b19f7a1a5491af05468bc3e88c8d1132
Miri Meltzer <miri@gizra.com>
2015-01-14 13:25:27 +0200

    Indentation
    commit aaf938a5ebcecf6c59a304883f552b1c0a5e9c3d
    Merge: f295a98 f64d93b
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Tue Jan 13 09:31:50 2015 +0200
    
        Merge branch '45' of github.com:Gizra/haskala into 45
    
    commit f295a98ae0e6e55f9f57f9466cec42b071a82dd7
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Tue Jan 13 09:30:54 2015 +0200
    
        Ctools plugin
    
    commit f64d93bc600ff882154dd0953118ec8b87061202
    Merge: 4f8a7fd c8a948c
    Author: Itamar Shapira Bar-Lev <bitamar@gmail.com>
    Date:   Mon Jan 12 23:33:19 2015 +0200
    
        Merge branch 'master' into 45
    
    commit 4f8a7fd364a026e75fd676dcfc5515428c945737
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 16:03:31 2015 +0200
    
        node_view:Book page
    
    commit 5547026d8f6ff07dcc4402886d635387ead50f06
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 15:26:27 2015 +0200
    
        Plugin fix
    
    commit f1ca9883f0b4aadd3644975b727aaa82133e52aa
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 14:43:13 2015 +0200
    
        Ctools plugin
    
    commit 69732def4f35e0fd599a620e7dba8b6157654b21
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 11:44:07 2015 +0200
    
        Starting views
    
    commit 9c47eadbdf916e07dee751bb6c738b4707ff2a2b
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 09:47:08 2015 +0200
    
        ADD panels node view variable
    
    commit 4759cac8d1d0ad74efb48281fea8715e75c90aaa
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 09:33:02 2015 +0200
    
        css fix
    
    commit a915bb012291fd912a942a5aa943fa41ecdff6c8
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 08:57:02 2015 +0200
    
        Footer nav
    
    commit 4390afe1a2e05908436be794cab9a1923ba254f5
    Merge: 28ee12f a6df3c1
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 07:58:28 2015 +0200
    
        Merge branch 'master' into 45
    
    commit 28ee12f4698750dea89ce9b73558513765a5cf09
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Mon Jan 12 07:55:00 2015 +0200
    
        CSS
    
    commit 5b6a3febd0d1a58f932f2e237c6405e138655bec
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Sun Jan 11 14:48:50 2015 +0200
    
        Design continue
    
    commit 8dafb747ab3942aeae66ed42d75372602b3c62c7
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Sun Jan 11 13:53:45 2015 +0200
    
        Design
    
    commit 18af04e9c6ba54e22a494e23d40c48cc00da9fcd
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Sun Jan 11 11:04:11 2015 +0200
    
        Field group 2
    
    commit 185d3ab585599e1fc4ca8177e8ef551ebb2fc49f
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Sun Jan 11 10:07:47 2015 +0200
    
        Group fields
    
    commit 771ef355ca60b0157945e21ca6d5f418e508ba1d
    Author: Miri Meltzer <miri@gizra.com>
    Date:   Thu Jan 8 13:16:35 2015 +0200
    
        Starting implement book page

--- haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
@@ -12,7 +12,7 @@
  */
 ?>
 
-      <section class="top-details">
+      <section id="top-details">
         <?php if (!empty($title)): ?>
           <h1 class="title page-header"><?php print $title; ?></h1>
         <?php endif; ?>
@@ -22,20 +22,24 @@
             <div class="field-label type-label">Type of book:</div>
             <div class="content"></div>
           </div>
+          <div class="field">
+            <div class="field-label">Full title of book:</div>
+            <div class="field-items"><?php print $title; ?></div>
+          </div>
           <?php foreach($top_details_group as $field):
             print render($field);
           endforeach; ?>
         </div>
       </section>
-      <section class="bottom-details">
-        <nav class="details-navigation">
+      <section id="bottom-details">
+        <nav id="details-navigation">
           <ul>
             <?php foreach($tabs as $group_name => $group): ?>
                 <li><a href="#<?php print $group_name; ?>"><?php print $group_name; ?></a></li>
             <?php endforeach; ?>
           </ul>
         </nav>
-        <section class="groups">
+        <section id="groups">
           <?php foreach($tabs as $tab_name => $tab_fields): ?>
               <div class="fields-group">
                 <div class="fields-group-name" id="<?php print $tab_name; ?>"><?php print $tab_name; ?></div>
