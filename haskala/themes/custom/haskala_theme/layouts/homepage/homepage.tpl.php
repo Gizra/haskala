54b40da2135c6cef1e75e19bb8459b6c9b24c897
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-01-27 12:18:43 +0200

    #55: Adding topics cloud

--- haskala/themes/custom/haskala_theme/layouts/homepage/homepage.tpl.php
+++ haskala/themes/custom/haskala_theme/layouts/homepage/homepage.tpl.php
@@ -10,40 +10,10 @@
 </section>
 
 <section class="tags-cloud-wrapper">
-  <h3>TAG CLOUD BY TOPICS</h3>
+  <h3><?php print t('Tag cloud by topics'); ?></h3>
   <div class="tags-cloud">
     <nav class="nav-bar">
-      <ul>
-        <li><a href="tags/ipsum">ipsum</a></li>
-        <li><a href="tags/ipsum">dolor</a></li>
-        <li><a href="tags/ipsum">sic</a></li>
-        <li><a href="tags/ipsum">amet</a></li>
-        <li><a href="tags/ipsum">consectetur</a></li>
-        <li><a href="tags/ipsum">adipiscing</a></li>
-        <li><a href="tags/ipsum">elit</a></li>
-        <li><a href="tags/ipsum">Phsellus</a></li>
-        <li><a href="tags/ipsum">tincidunt</a></li>
-        <li><a href="tags/ipsum">idunt</a></li>
-        <li><a href="tags/ipsum">congue</a></li>
-        <li><a href="tags/ipsum">massa scelerisque utsit</a></li>
-        <li><a href="tags/ipsum">dictum sagittis</a></li>
-        <li><a href="tags/ipsum">sit</a></li>
-        <li><a href="tags/ipsum">dipsic</a></li>
-        <li><a href="tags/ipsum">egestas</a></li>
-        <li><a href="tags/ipsum">elit</a></li>
-        <li><a href="tags/ipsum">suspic a non</a></li>
-        <li><a href="tags/ipsum">dictum</a></li>
-        <li><a href="tags/ipsum">arcu</a></li>
-        <li><a href="tags/ipsum">Nulla ut erat</a></li>
-        <li><a href="tags/ipsum">amet</a></li>
-        <li><a href="tags/ipsum">arcu</a></li>
-        <li><a href="tags/ipsum">dolor</a></li>
-        <li><a href="tags/ipsum">Cras nec dsghfjgnibh</a></li>
-        <li><a href="tags/ipsum">vitae tellus</a></li>
-        <li><a href="tags/ipsum">dictum sagittis</a></li>
-        <li><a href="tags/ipsum">ecteture</a></li>
-        <li><a href="tags/ipsum">dui vitae</a></li>
-      </ul>
+      <?php print $content['topics']; ?>
     </nav>
   </div>
 </section>
