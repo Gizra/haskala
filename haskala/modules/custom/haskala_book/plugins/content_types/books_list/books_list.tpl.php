cac974a158074f33d1fad805cfe654125a9417ec
Helena Eksler <helena@gizra.com>
2015-01-27 12:42:02 +0200

    Class name.

--- haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
@@ -1,3 +1,4 @@
-<div class="letter-katalog">
-  Here will be alphabet.
-</div>
+<section class="books-filter">
+  <nav class="eng"><?php print $alpabet_en; ?></nav>
+  <nav class="he"><?php print $alpabet_he; ?></nav>
+</section>
