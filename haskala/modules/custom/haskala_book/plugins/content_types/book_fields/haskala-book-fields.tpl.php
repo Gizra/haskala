bfe2be679110b9eccb754cd54fc8f3cb3bed81a0
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-01-27 10:43:39 +0200

    #73: Book page - Producers tab

--- haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/book_fields/haskala-book-fields.tpl.php
@@ -17,19 +17,17 @@
     <h1 class="title page-header"><?php print $title; ?></h1>
   <?php endif; ?>
 
-  <div class="top-fields">
-    <div class="field">
-      <div class="field-label type-label">Type of book:</div>
-      <div class="content"></div>
+  <?php if (!empty($top_details_group)): ?>
+    <div class="top-fields">
+      <div class="field">
+        <div class="field-label type-label">Type of book:</div>
+        <div class="content"></div>
+      </div>
+      <?php foreach($top_details_group as $field):
+        print render($field);
+      endforeach; ?>
     </div>
-    <div class="field">
-      <div class="field-label">Full title of book:</div>
-      <div class="field-items"><?php print $title; ?></div>
-    </div>
-    <?php foreach($top_details_group as $field):
-      print render($field);
-    endforeach; ?>
-  </div>
+  <?php endif; ?>
 </section>
 
 <section id="bottom-details">
@@ -41,15 +39,19 @@
     </ul>
   </nav>
   <section id="groups">
-    <?php foreach($tabs as $tab_name => $tab_fields): ?>
-        <div class="fields-group">
-          <div class="fields-group-name" id="<?php print $tab_name; ?>"><?php print $tab_name; ?></div>
-          <div class="fields">
-            <?php foreach($tab_fields as $tab_field): ?>
-              <?php print render($tab_field); ?>
-            <?php endforeach; ?>
-          </div>
+    <?php foreach($tabs as $tab_name => $multi_tab_fields): ?>
+      <div class="fields-group">
+        <div class="fields-group-name" id="<?php print $tab_name; ?>"><?php print $tab_name; ?></div>
+        <div class="fields">
+          <?php foreach($multi_tab_fields as $single_tab_fields): ?>
+            <div class="fields-multi">
+              <?php foreach($single_tab_fields as $tab_field): ?>
+                <?php print render($tab_field); ?>
+              <?php endforeach; ?>
+            </div>
+          <?php endforeach; ?>
         </div>
+      </div>
     <?php endforeach; ?>
   </section>
 </section>
