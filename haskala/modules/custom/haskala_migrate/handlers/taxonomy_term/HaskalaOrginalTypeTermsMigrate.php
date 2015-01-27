14b22386835cc06470609148d7a4eae57faf9daf
Miri Meltzer <miri@gizra.com>
2015-01-27 12:52:32 +0200

    Orginal types terms migration

---
+++ haskala/modules/custom/haskala_migrate/handlers/taxonomy_term/HaskalaOrginalTypeTermsMigrate.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \HaskalaOrginalTypeTermsMigrate.
+ */
+
+class HaskalaOrginalTypeTermsMigrate extends HaskalaMigration {
+  public $entityType = 'taxonomy_term';
+  public $bundle = 'orginal_types';
+}
+
