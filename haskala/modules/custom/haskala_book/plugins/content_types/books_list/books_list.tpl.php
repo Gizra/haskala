6f5c624b70e418bc46921a916ee15918479aef04
Helena Eksler <helena@gizra.com>
2015-01-27 16:17:15 +0200

    Books page ready.

--- haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
@@ -2,3 +2,6 @@
   <nav class="eng"><?php print $alphabet_en; ?></nav>
   <nav class="he"><?php print $alphabet_he; ?></nav>
 </section>
+<section class="books-list">
+  <?php print $books_blocks; ?>
+</section>
---
+++ haskala/modules/custom/haskala_book/plugins/content_types/books_list/letter_books_list.tpl.php
@@ -0,0 +1,6 @@
+<div class="letter-wrapper">
+  <div class="letter"><span><?php print $letter; ?></span></div>
+  <div class="books-by-letter">
+    <?php print $books_list; ?>
+  </div>
+</div>
