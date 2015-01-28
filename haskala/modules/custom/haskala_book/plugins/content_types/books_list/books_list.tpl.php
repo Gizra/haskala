5a2a3b4b668036c5dcfd4bcf77b8ce33458049b7
Helena Eksler <helena@gizra.com>
2015-01-28 10:13:52 +0200

    Filter moved to separate plugin. Do not show empty letters

--- haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
@@ -1,7 +1,3 @@
-<section class="books-filter">
-  <nav class="eng"><?php print $alphabet_en; ?></nav>
-  <nav class="he"><?php print $alphabet_he; ?></nav>
-</section>
 <section class="books-list">
   <?php print $books_blocks; ?>
 </section>
