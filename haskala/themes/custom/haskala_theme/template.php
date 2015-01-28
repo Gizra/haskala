e9ef5f3eb90e4255a44fab4b8937f102c818ab4c
Helena Eksler <helena@gizra.com>
2015-01-28 16:12:55 +0200

    Fixed showing person name in different languages.

--- haskala/themes/custom/haskala_theme/template.php
+++ haskala/themes/custom/haskala_theme/template.php
@@ -23,7 +23,6 @@ function haskala_theme_preprocess_node(&$variables) {
   if (function_exists($preprocess_function)) {
     $preprocess_function($variables);
   }
-
   $preprocess_function = "haskala_theme_preprocess_node__{$node->type}__{$view_mode}";
   if (function_exists($preprocess_function)) {
     $preprocess_function($variables);
@@ -31,10 +30,30 @@ function haskala_theme_preprocess_node(&$variables) {
 }
 
 /**
- * Preprocess for article images.
+ * Preprocess for book nodes teaser.
  */
 function haskala_theme_preprocess_node__book__teaser(&$variables) {
   $node = $variables['node'];
   $variables['url'] = url('node/' . $node->nid);
 }
+
+/**
+ * Preprocess for peron nodes teaser.
+ */
+function haskala_theme_preprocess_node__person(&$variables) {
+  $node = $variables['node'];
+  $view_mode = $variables['view_mode'];
+  if ($view_mode == 'teaser' || $view_mode == 'teaser_en' || $view_mode == 'teaser_he') {
+    // Generic tpl for node--person--teaser.
+    $variables['theme_hook_suggestions'][] = "node__person__teaser";
+    $variables['url'] = url('node/' . $node->nid);
+  }
+  if ($view_mode == 'teaser' || $view_mode == 'teaser_en') {
+    $variables['name'] = entity_metadata_wrapper('node', $node)->field_german_name->value();
+  }
+  else if ($view_mode == 'teaser_he') {
+    $variables['name'] = entity_metadata_wrapper('node', $node)->field_hebrew_name->value();
+  }
+}
+
 
