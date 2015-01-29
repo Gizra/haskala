7378c36a9df1ffa1745b415b0aeb16ef61dbb8c0
Helena Eksler <helena@gizra.com>
2015-01-29 10:21:16 +0200

    Fixes in drupal template.

--- haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
+++ haskala/modules/custom/haskala_book/plugins/content_types/books_list/books_list.tpl.php
@@ -1,3 +1,7 @@
 <section class="books-list">
+  <div class="results-message">
+<!--  This part will be dynamic and only for topics pages.  -->
+    The following 200 pages are in this category, out of 525 total.
+  </div>
   <?php print $books_blocks; ?>
 </section>
