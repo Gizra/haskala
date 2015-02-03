e24d023d56432c4e6feb25bf32f2da1dcd689ca1
DavidGizra <david@gizra.com>
2015-02-03 17:40:53 +0200

    Update the url.

--- phantomcss/homepage-drupal.js
+++ phantomcss/homepage-drupal.js
@@ -6,7 +6,7 @@ phantomcss.init({
   libraryRoot: './node_modules/phantomcss'
 });
 
-casper.start('http://localhost/haskala/www/');
+casper.start('http://127.0.0.1:8080');
 
 casper.viewport(1024, 768);
 
