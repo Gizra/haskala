855e2e935892218348b6dd274db3cbd81b4aafbf
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-02-04 17:03:58 +0200

    Migrating religion terms

---
+++ haskala/modules/custom/haskala_migrate/handlers/taxonomy_term/HaskalaReligionTermsMigrate.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \HaskalaReligionTermsMigrate.
+ */
+
+class HaskalaReligionTermsMigrate extends HaskalaMigration {
+  public $entityType = 'taxonomy_term';
+  public $bundle = 'religion';
+}
+
