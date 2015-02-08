ea148a3afbbee028dbfdee2860cb39fdc838fa6e
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-02-08 16:01:56 +0200

    Disabling scroller links when irrelevant

--- jekyll/app/js/main.js
+++ jekyll/app/js/main.js
@@ -6,22 +6,30 @@ $(document).ready(function() {
     // The increments are half the tabs container width.
     var visibleWidth = $('#details-navigation').width();
     var delta = visibleWidth / 2;
-    if ($(event.currentTarget).data('direction') == 'next') {
+    var a = $(event.currentTarget);
+    if (a.data('direction') == 'next') {
       delta *= -1;
     }
 
     // Re-set the list margin.
     var margin = parseInt($('#details-navigation ul').css('margin-left'));
     margin += delta;
+
+    // Enable both arrow links, and disable one of them in case a limit was reached.
+    $('#details-navigation > a').removeClass('disabled');
+
     // Block left navigation on zero.
     if (margin > 0) {
       margin = 0;
+      a.addClass('disabled');
     }
+
     // Block right navigation when the tab list fills less than half the container.
     var listWidth = $('#details-navigation ul').width();
     var minMargin = (listWidth - (visibleWidth / 2)) * -1;
     if (margin < minMargin ) {
       margin = minMargin;
+      a.addClass('disabled');
     }
     // Update the list margin.
     $('#details-navigation ul').css('margin-left', margin + "px");
