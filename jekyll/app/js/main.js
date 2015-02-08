6cca2773dbc5b47c8ded20fbb6d92254a0b09cec
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-02-08 15:43:16 +0200

    Implementing scroller

--- jekyll/app/js/main.js
+++ jekyll/app/js/main.js
@@ -1,1 +1,29 @@
-alert('hoy');
+// Book tabs scroller.
+$(document).ready(function() {
+
+  $('#details-navigation > a').click(function(event) {
+    event.preventDefault();
+    // The increments are half the tabs container width.
+    var visibleWidth = $('#details-navigation').width();
+    var delta = visibleWidth / 2;
+    if ($(event.currentTarget).data('direction') == 'next') {
+      delta *= -1;
+    }
+
+    // Re-set the list margin.
+    var margin = parseInt($('#details-navigation ul').css('margin-left'));
+    margin += delta;
+    // Block left navigation on zero.
+    if (margin > 0) {
+      margin = 0;
+    }
+    // Block right navigation when the tab list fills less than half the container.
+    var listWidth = $('#details-navigation ul').width();
+    var minMargin = (listWidth - (visibleWidth / 2)) * -1;
+    if (margin < minMargin ) {
+      margin = minMargin;
+    }
+    // Update the list margin.
+    $('#details-navigation ul').css('margin-left', margin + "px");
+  });
+});
