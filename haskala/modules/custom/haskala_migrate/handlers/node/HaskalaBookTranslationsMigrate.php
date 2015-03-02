cb414c7521e0826630b67edf6553eb00502f8986
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-03-02 09:41:20 +0200

    Fixing translation migration, and displaying translations as nodes

--- haskala/modules/custom/haskala_migrate/handlers/node/HaskalaBookTranslationsMigrate.php
+++ haskala/modules/custom/haskala_migrate/handlers/node/HaskalaBookTranslationsMigrate.php
@@ -12,9 +12,8 @@ class HaskalaBookTranslationsMigrate extends HaskalaMigration {
     array('field_language', 'Language of translation'),
     array('field_translation_year', 'Year of translation'),
     array('field_translation_city', 'City of translation'),
-    array('field_translation_city', 'Translator of translation'),
-    array('field_translator', 'References for translation'),
-
+    array('field_translator', 'Translator of translation'),
+    array('field_translation_references', 'References for translation'),
   );
 
   public $dependencies = array(
@@ -44,7 +43,8 @@ class HaskalaBookTranslationsMigrate extends HaskalaMigration {
     $this->addFieldMapping('field_translation_city:ignore_case')
       ->defaultValue(TRUE);
 
-    $this->addFieldMapping('field_translator', 'field_translator');
+    $this->addFieldMapping('field_translator', 'field_translator')
+      ->sourceMigration('HaskalaPeopleMigrate');
 
     $this->addFieldMapping('field_translation_references', 'field_translation_references');
   }
@@ -55,10 +55,5 @@ class HaskalaBookTranslationsMigrate extends HaskalaMigration {
 
     // Add language to book translation title for prevent duplicated titles.
     $row->title .= ' - '.$row->field_language;
-
-    // Fetch person node IDs by their titles.
-    if ($row->field_translator) {
-      $row->field_translator = $this->getNodeByTitle('person', $row->field_translator);
-    }
   }
 }
